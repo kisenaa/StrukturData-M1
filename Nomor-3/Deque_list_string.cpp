2a92ccc1d2fd57b632236c99eb46b5e3ce14d381
Johannes D <115395772+kisenaa@users.noreply.github.com>
2023-03-07 11:35:48 +0700

    Create Deque_list_string.cpp

---
+++ Nomor-3/Deque_list_string.cpp
@@ -0,0 +1,155 @@
+/**
+ * Implementasi ADT Deque (Double-ended Queue)
+ * 
+ * Dibuat dan ditulis oleh Bayu Laksana
+ * -- tanggal 22 Januari 2019
+ * Struktur Data 2020
+ * Implementasi untuk bahasa C++
+ * 
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+using namespace std;
+
+// Node Structure
+struct DListNode {
+    char data[101];
+    DListNode *next, *prev;
+};
+
+struct Deque
+{
+    DListNode *_head, *_tail;
+    unsigned _size;
+
+    // Utility function
+    DListNode* __createNode(const char* value)
+    {
+        DListNode *newNode = \
+            (DListNode*) malloc (sizeof(DListNode));
+    
+        if (!newNode) return NULL;
+        strcpy(newNode->data, value);
+        newNode->next = NULL;
+        newNode->prev = NULL;
+
+        return (DListNode*) newNode;
+    }
+
+    void init() {
+        _head = _tail = NULL;
+        _size = 0;
+    }
+
+    bool isEmpty() {
+        return (_head == NULL && _tail == NULL);
+    }
+
+    void pushFront(const char*  value)
+    {
+        DListNode *newNode = __createNode(value);
+        if (newNode) {
+            _size++;
+            if (isEmpty()) {
+                _head = newNode;
+                _tail = newNode;
+                return;
+            }
+
+            newNode->next = _head;
+            _head->prev = newNode;
+            _head = newNode;
+        }
+    }
+
+    void pushBack(const char*  value)
+    {
+        DListNode *newNode = __createNode(value);
+        if (newNode) {
+            _size++;
+            if (isEmpty()) {
+                _head = newNode;
+                _tail = newNode;
+                return;
+            }
+
+            _tail->next = newNode;
+            newNode->prev = _tail;
+            _tail = newNode;
+        }
+    }
+
+    char* front()
+    {
+        if (!isEmpty())
+            return _head->data;
+        exit(-1);
+    }
+
+    char* back()
+    {
+        if (!isEmpty())
+            return _tail->data;
+        exit(-1);
+    }
+
+    void popFront()
+    {
+        if (!isEmpty()) {
+            DListNode *temp = _head;
+            if (_head == _tail) {
+                _head = NULL;
+                _tail = NULL;
+                free(temp);
+            }
+            else {
+                _head = _head->next;
+                _head->prev = NULL;
+                free(temp);
+            }
+            _size--;   
+        }
+    }
+
+    void popBack()
+    {
+        if (!isEmpty()) {
+            DListNode *temp;
+            if (_head == _tail) {
+                temp = _head;
+                _head = NULL;
+                _tail = NULL;
+                free(temp);
+            }
+            else {
+                temp = _tail;
+                _tail = _tail->prev;
+                _tail->next = NULL;
+                free(temp);
+            }
+            _size--;
+        }
+    }
+};
+
+int main(int argc, char const *argv[])
+{
+    Deque myDeque;
+
+    myDeque.init();
+
+    myDeque.pushBack("HELLO");
+    myDeque.pushBack("GOOD");
+    myDeque.pushBack("MONEY");
+
+     while (!myDeque.isEmpty()) {
+        printf("%s\n", myDeque.front());
+        myDeque.popFront();
+    }
+
+
+    return 0;
+}
