69be0fd789a33c5a8334bb2eb2c21645a5886e68
Johannes D <115395772+kisenaa@users.noreply.github.com>
2023-03-07 10:17:25 +0700

    Create Klinik_TC.c

---
+++ Nomor-2/Klinik_TC.c
@@ -0,0 +1,141 @@
+/**
+ * Implementasi ADT PriorityQueue menggunakan Linked List
+ * [default: minimum priority]
+ * 
+ * Dibuat dan ditulis oleh Bayu Laksana
+ * -- tanggal 22 Januari 2019
+ * Struktur Data 2020
+ * Implementasi untuk bahasa C
+ * 
+ * !!NOTE!!
+ * cara menggunakan lihat pada fungsi main()
+ */
+
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+
+/* Struktur Node */
+
+typedef struct pqueueNode_t {
+    int data;
+    int id;
+    struct pqueueNode_t *next;
+} PQueueNode;
+
+/* Struktur ADT PriorityQueue menggunakan Linked List */
+
+// Prioritas default: nilai minimum
+typedef struct pqueue_t {
+    PQueueNode *_top;
+    unsigned _size;
+} PriorityQueue;
+
+/* Function prototype */
+
+void pqueue_init(PriorityQueue *pqueue);
+bool pqueue_isEmpty(PriorityQueue *pqueue);
+void pqueue_push(PriorityQueue *pqueue, int value, int number);
+void pqueue_pop(PriorityQueue *pqueue);
+int  pqueue_top(PriorityQueue *pqueue);
+int get_ID(PriorityQueue *pqueue);
+
+/* Function definition below */
+
+void pqueue_init(PriorityQueue *pqueue)
+{
+    pqueue->_top = NULL;
+    pqueue->_size = 0;
+}
+
+bool pqueue_isEmpty(PriorityQueue *pqueue) {
+    return (pqueue->_top == NULL);
+}
+
+void pqueue_push(PriorityQueue *pqueue, int value, int number)
+{
+    PQueueNode *temp = pqueue->_top;
+    PQueueNode *newNode = \
+        (PQueueNode*) malloc (sizeof(PQueueNode));
+    newNode->data = value;
+    newNode->id = number;
+    newNode->next = NULL;
+
+    if (pqueue_isEmpty(pqueue)) {
+        pqueue->_top = newNode;
+        return;
+    }
+
+    if (value > pqueue->_top->data) {
+        newNode->next = pqueue->_top;
+        pqueue->_top = newNode;
+    }
+    else {
+        while ( temp->next != NULL && 
+                temp->next->data > value)
+            temp = temp->next;
+        newNode->next = temp->next;
+        temp->next = newNode;
+    }
+}
+
+void pqueue_pop(PriorityQueue *pqueue)
+{
+    if (!pqueue_isEmpty(pqueue)) {
+        PQueueNode *temp = pqueue->_top;
+        pqueue->_top = pqueue->_top->next;
+        free(temp);
+    }
+}
+
+int pqueue_top(PriorityQueue *pqueue) {
+    if (!pqueue_isEmpty(pqueue))
+        return pqueue->_top->data;
+    else return 0;
+}
+
+int get_ID(PriorityQueue *pqueue) {
+    if (!pqueue_isEmpty(pqueue))
+        return pqueue->_top->id;
+    else return 0;
+}
+
+int main(int argc, char const *argv[])
+{
+    // Buat objek PriorityQueue
+    PriorityQueue myPque;
+
+    // PENTING! Jangan lupa diinisialisasi
+    pqueue_init(&myPque);
+
+
+    int number;
+    scanf("%d", &number);
+
+    char str[7];
+
+    for (int i=0; i<number; i++) {
+        int val,num;
+        scanf(" %s", str);
+
+        if (strcmp ("MASUK", str) == 0) {
+            scanf(" %d %d", &val, &num);
+            pqueue_push(&myPque, num, val);
+        }
+        else {
+            if(!pqueue_isEmpty(&myPque)) {
+            printf("%d\n", get_ID(&myPque));
+            pqueue_pop(&myPque);
+            }
+        }
+
+    }
+
+    if(pqueue_isEmpty(&myPque)) {
+            printf("HORE");
+        }
+
+    
+    return 0;
+}
